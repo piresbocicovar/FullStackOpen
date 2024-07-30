437cb0cd80f4b7f24243eed8d7876f522e56ed17
piresbocicovar <piresbocicovar@gmail.com>
2024-07-30 19:30:03 -0300

    exs. 4.6 & 4.7 + moved all tests to a single file

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -1,40 +1,52 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
 
 const totalLikes = (blogs) => {
-    return(
-        blogs.reduce(
-            (accumulator, currentValue) => accumulator + currentValue.likes,
-            0
-        )
-    )
+    return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
 }
 
 const favoriteBlog = (blogs) => {
-    return(
-        blogs.reduce(
-            (previous, current) => {
-                return (
-                    (previous && previous.likes > current.likes) ? 
-                    {
-                        title: previous.title,
-                        author: previous.author,
-                        likes: previous.likes
-                    }
-                    :
-                    {
-                        title: current.title,
-                        author: current.author,
-                        likes: current.likes
-                    }
-                )
-            },
-            {}
-        )
-    )
+    if (blogs.length === 0) {
+        return {}
+    }
+
+    const favorite = blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
+
+    return {
+        author: favorite.author,
+        title: favorite.title,
+        likes: favorite.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    const authors = _.groupBy(blogs, 'author');
+
+    const authorBlogs = _.map(authors, (blogs, author) => ({
+        author,
+        blogs: blogs.length
+    }));
+
+    const result = _.maxBy(authorBlogs, 'blogs');
+    return result || {};
+};
+
+
+const mostLikes = (blogs) => {
+    const authors = _.groupBy(blogs, 'author')
+
+    const authorLikes = _.map(authors, (blogs, author) => ({
+        author,
+        likes: _.sumBy(blogs, 'likes')
+    }))
+
+    const result = _.maxBy(authorLikes, 'likes')
+    return result || {}
 }
 
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
