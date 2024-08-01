7d150e4396a4194ab0bfd984d989a3630d658fa8
piresbocicovar <piresbocicovar@gmail.com>
2024-08-01 19:13:22 -0300

    fixed missing "response" parameter

--- Part4/bloglist/utils/middleware.js
+++ Part4/bloglist/utils/middleware.js
@@ -1,6 +1,6 @@
 const logger = require('./logger')
 
-const requestLogger = (request, next) => {
+const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
