0280c3280be5c5d67bb4840c6ae5fe5dc634b4de
piresbocicovar <piresbocicovar@gmail.com>
2024-09-10 14:59:33 -0300

    exercises 7.4 ~ 7.6

--- Part7/routed-anecdotes/src/components/CreateNew.jsx
+++ Part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,23 +1,33 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import PropTypes from "prop-types"
+import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author  = useField('text')
+  const info = useField('text')
 
   const navigate = useNavigate()
+
+  // eslint-disable-next-line no-unused-vars
+  const filterProps = ({ clear, ...rest }) => rest
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.clear()
+    author.clear()
+    info.clear()
   }
 
   return (
@@ -26,17 +36,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...filterProps(content)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...filterProps(author)} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...filterProps(info)} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
